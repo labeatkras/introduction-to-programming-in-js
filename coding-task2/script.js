f9a25318febf4662f042c908684b8ac0ed29b2f4
Labeat Krasniqi <labeat.kras@gmail.com>
2023-03-21 23:27:49 +0100

    coding task 2, operators & data types

---
+++ coding-task2/script.js
@@ -0,0 +1,16 @@
+console.log("Labeat");
+
+console.log("Hallo");
+
+console.log("That" + " doesn`t " + "work");
+
+console.log(50 - 2 < 52);
+
+console.log("333" === 333);
+
+console.log((600 + 510) / 2);
+
+console.log(2 + 2);
+
+console.log(25 / 5 === 5); // Result: 5
+console.log(50 / 10 === 5); // Result: 5
--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 console.log("Hello World");
+
+//Arithmetic operators
+
+console.log(120 + 180);
+console.log(20 - 5);
+console.log(6 * 6);
+console.log(30 / 3);
+
+//Comparsion Operators
+
+//Equal
+console.log(1 == 1);
+//Not Equal
+console.log(1 != 1);
+//Greater than
+console.log(1 > 1);
+//Greater than or equal
+console.log(1 >= 1);
+//Less than
+console.log(1 < 1);
+//Less than or equal
+console.log(1 <= 1);
+
+/*Strict equal operator 
+use it like that and not the equal or not equal*/
+console.log(1 === 1);
+console.log(1 !== 2);
+
+//Logical Operators
+
+//AND
+console.log(2 > 1 && 7 < 6);
+
+//OR
+console.log(0 > 1 || 7 < 6);
+
+//NOT
+console.log(!(5 > 10));
+
+/*Grouping Operator 
+wird immer vorgezogen  ( )*/
+// Beispiel
+
+console.log(7 + 20 / 5);
+//Result: 11 (mathematische Reihenfolge * vor +)
+
+console.log((7 + 20) / 5);
+//Result 5.4 (Grouping Operator ( ) geht vor)
+
+//Concatenation Operator Strings
+
+console.log("My Name is Labi");
+//Result: "My Name is Labi"
+
+console.log("My" + " Name" + " is Labi");
+/*Result: "My Name is Labi" 
+ "(leerzeichen) is Labi" trennt die Strings*/
+
+console.log("1" + 1);
+//Result: 11
+
+console.log("" + 6 + 8);
+//Result: 68 (ergibt immer ein string " " hat vorang)
+
+console.log("La" + "beat" === "Labeat");
+//Result: true
